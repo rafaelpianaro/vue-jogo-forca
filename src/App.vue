078b5e81350f7ca1ae49a9735401d6feafee3b67
Rafael <rafaelpianaro@gmail.com>
2022-02-13 10:42:53 -0300

    *sections

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <div id="app">
+    <h1>Jogo da Forca</h1>
+    <section id="inicio">
+      Início
+    </section>
+    <section id="jogo">
+      Jogo
+    </section>
   </div>
 </template>
 
 <script>
+import './css/global.css'
 
 export default {
   name: 'App',
@@ -15,6 +23,11 @@ export default {
 
 <style>
 #app {
-  
+  width: 100%;
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 </style>
