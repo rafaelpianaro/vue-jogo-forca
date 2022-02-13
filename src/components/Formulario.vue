e06894bf2650385664ed0432cbd84b769287d28d
Rafael <rafaelpianaro@gmail.com>
2022-02-13 16:51:02 -0300

    *style para Formulario.vue

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-
+  <div class="formulario">
+      <div class="formulario-title">Defina a palavra</div>
+      <input type="text" class="formulario-input">
+      <button>Próximo</button>
   </div>
 </template>
 
@@ -11,5 +13,21 @@ export default {
 </script>
 
 <style>
+    .formulario {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+        justify-content: center;
+        margin-top: 20px;
+    }
 
+    .formulario-input {
+        padding: 10px 5px;
+        border: 0;
+        border-radius: 5px;
+        margin: 10px 0;
+        text-align: center;
+        color: var(--color-text-dark);
+    }
 </style>
