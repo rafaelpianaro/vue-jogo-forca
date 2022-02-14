fd43e4923b20ae9c81a2b01d70b70ed0dffdcbd4
Rafael Pianaro <rafael_pianaro@econeteditora.com.br>
2022-02-14 12:54:12 -0300

    *style para .palavra*

--- src/components/Palavra.vue
+++ src/components/Palavra.vue
@@ -35,4 +35,20 @@ export default {
     text-align: center;
     align-items: center;
   }
+
+  .palavra-letras {
+    display: flex;
+    margin-bottom: 20px;
+  }
+
+  .palavra-letra {
+    height: 30px;
+    width: 30px;
+    margin: 0px 5px;
+    border-bottom: 1px solid var(--color-text-light);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
 </style>
